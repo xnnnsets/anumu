7bbb5e161bf37ff22ba3383cad1e6daa2d6545e2
xnnnsets <167276632+xnnnsets@users.noreply.github.com>
2024-05-19 14:19:54 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/xnnnsets/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/xnnnsets/anumu/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
