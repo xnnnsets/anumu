c84b6bc9ab54cae68b576546c4ffb9036b7c3eb9
xnnnsets <167276632+xnnnsets@users.noreply.github.com>
2024-05-19 16:08:18 +0700

    Update limit.sh

--- ssh/limit.sh
+++ ssh/limit.sh
@@ -3,19 +3,23 @@ wget -q -O /etc/systemd/system/limitvmess.service "${REPO}xray/limitvmess.servic
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}xray/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}xray/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}xray/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/socks.service "${REPO}xray/socks.service" && chmod +x socks.service >/dev/null 2>&1
 wget -q -O /etc/xray/limit.vmess "${REPO}xray/vmess" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.vless "${REPO}xray/vless" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.trojan "${REPO}xray/trojan" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.shadowsocks "${REPO}xray/shadowsocks" >/dev/null 2>&1
+wget -q -O /usr/bin/ws.py "${REPO}ssh/ws.py" >/dev/null 2>&1
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
 chmod +x /etc/xray/limit.shadowsocks
+chmod +x /etc/usr/bin/ws.py
 systemctl daemon-reload
 systemctl enable --now limitvmess
 systemctl enable --now limitvless
 systemctl enable --now limittrojan
 systemctl enable --now limitshadowsocks
+systemctl enable --now socks.service
 # systemctl start limitvmess
 # systemctl start limitvless
 # systemctl start limittrojan
